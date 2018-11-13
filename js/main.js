54eb6a1309e178fef0233a1ef54f426ad5c333af
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2018-11-13 10:02:42 +0000

    Making navbar sticky;  ES6 and eslint on the javascript

--- js/main.js
+++ js/main.js
@@ -1,18 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-	"use strict";
-	let sched = later.parse.recur().on(7).dayOfWeek().on(2, 4).dayOfWeekCount();
-	let nextThree = later.schedule(sched).next(3);
-	let options = {
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const sched = later.parse.recur().on(7).dayOfWeek().on(2, 4)
+		.dayOfWeekCount();
+	const nextThree = later.schedule(sched).next(3);
+	const options = {
 		month: "long",
-		day: "numeric"
+		day: "numeric",
 	};
-	let firstNext = (nextThree[0].toLocaleDateString("en-GB", options));
-	let secondNext = (nextThree[1].toLocaleDateString("en-GB", options));
-	let thirdNext = (nextThree[2].toLocaleDateString("en-GB", options));
-	let upcomingsingings = "<p>Our next singings are on: <span class=\"font-italic font-weight-bold\"><span id=\"next-singing\">Saturday " +
-        firstNext + "</span><span class=\"font-weight-normal\">, </span><span id=\"second-next-singing\">" +
-        secondNext +
-        "</span><span class=\"font-weight-normal\">, and </span><span id=\"third-next-singing\">" + thirdNext +
-        "</span><span class=\"font-weight-normal\">.</span></span></p>";
+	const firstNext = (nextThree[0].toLocaleDateString("en-GB", options));
+	const secondNext = (nextThree[1].toLocaleDateString("en-GB", options));
+	const thirdNext = (nextThree[2].toLocaleDateString("en-GB", options));
+	const upcomingsingings = `<p>Our next singings are on: <span class="font-italic font-weight-bold"><span id="next-singing">Saturday ${firstNext}</span><span class="font-weight-normal">, </span><span id="second-next-singing"> ${secondNext}</span><span class="font-weight-normal">, and </span><span id="third-next-singing"> ${thirdNext}</span><span class="font-weight-normal">.</span></span></p>`;
 	document.getElementById("upcoming-singings").innerHTML = upcomingsingings;
+});
+
+// Hides menu when clicked
+document.addEventListener("DOMContentLoaded", () => {
+	const nav_items = document.getElementsByClassName("nav-item");
+	const navbarButton = document.getElementsByClassName("navbar-toggler")[0];
+	const navbarToggler = document.getElementById("navbarToggler");
+	const main = document.getElementById("main");
+
+	Array.from(nav_items).forEach((nav_item) => {
+		nav_item.addEventListener("click", () => {
+			if (navbarToggler.classList.contains("show")) {
+				navbarButton.click();
+			}
+		});
+	});
+
+	main.addEventListener("click", () => {
+		if (navbarToggler.classList.contains("show")) {
+			navbarButton.click();
+		}
+	});
+});
+
+window.addEventListener("hashchange", () => {
+	scrollBy(0, -57);
 });
